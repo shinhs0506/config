60052545c6773d6d376455ba6b2343c95f70489f
John Shin <shinhs0506@gmail.com>
2023-11-11 11:55:49 -0800

    nvim: telescope update

--- nvim/init.lua
+++ nvim/init.lua
@@ -83,7 +83,7 @@ local function setupPlugins()
       end,
   }, {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
+    tag = '0.1.4',
     requires = { 'nvim-lua/plenary.nvim' },
   }, {
     'windwp/nvim-autopairs',
